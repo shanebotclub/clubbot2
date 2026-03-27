1c4059a9b7ec67049984fa1383203a61100af25b
shanebotclub <shaned75@googlemail.com>
2026-03-27 21:57:14 +0000

    add launch file to setup

--- src/clubbot2/setup.py
+++ src/clubbot2/setup.py
@@ -6,11 +6,13 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+   data_files=[
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch', ['launch/button_led_system.launch.py']),
+],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
