b203c05ae3397c02ce065585ce997375b97bc40b
shanebotclub <shaned75@googlemail.com>
2026-04-05 21:31:49 +0100

    added encoders to reader node

--- src/clubbot2/clubbot2/esp32_reader.py
+++ src/clubbot2/clubbot2/esp32_reader.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from clubbot_interfaces.msg import BumperStates
+from clubbot_interfaces.msg import EncoderCounts
 
 import serial
 
@@ -13,8 +14,9 @@ class Esp32Reader(Node):
 
         # Publishers
         self.bump_pub = self.create_publisher(BumperStates, 'bumpers', 10)
+        self.enc_pub  = self.create_publisher(EncoderCounts, 'encoders', 10)
 
-        # Timer to poll serial
+        # Timer to listen for serial
         self.timer = self.create_timer(0.01, self.read_serial)
 
         self.get_logger().info('esp32_reader started')
@@ -36,10 +38,14 @@ class Esp32Reader(Node):
 
         if msg_type == 'BUMP':
             self.handle_bump(parts[1:])
+        
+        elif msg_type == 'ENC':
+            self.handle_enc(parts[1:])
+        
         else:
-            # Future: handle ENC, IMU, etc.
             self.get_logger().debug(f'Unknown msg type: {msg_type}')
 
+    # Bumper message handler
     def handle_bump(self, fields):
         # fields: ["lf=1", "mf=0", ...]
         values = {}
@@ -63,6 +69,34 @@ class Esp32Reader(Node):
             return
 
         self.bump_pub.publish(msg)
+
+    # Encoder message handler
+    def handle_enc(self, fields):
+        # Example fields:
+        # ["left_forward=123", "left_backward=0", "right_forward=98", "right_backward=1"]
+
+        values = {}
+        for f in fields:
+            if '=' not in f:
+                continue
+            name, val = f.split('=', 1)
+            try:
+                values[name] = int(val)
+            except ValueError:
+                self.get_logger().warn(f'Bad encoder value: {f}')
+                return
+
+        msg = EncoderCounts()
+        try:
+            msg.left_forward  = values['left_forward']
+            msg.left_backward = values['left_backward']
+            msg.right_forward = values['right_forward']
+            msg.right_backward = values['right_backward']
+        except KeyError as e:
+            self.get_logger().warn(f'Missing encoder field: {e}')
+            return
+
+        self.enc_pub.publish(msg)
 
 
 def main(args=None):
