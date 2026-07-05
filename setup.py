8c289502f82de6b8cf121179ea03e9699d689e98
shanebotclub <shaned75@googlemail.com>
2026-07-05 23:17:39 +0100

    updated setup py

--- setup.py
+++ setup.py
@@ -1,37 +1,32 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 package_name = 'clubbot2'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-   data_files=[
-    ('share/ament_index/resource_index/packages',
-        ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    ('share/' + package_name + '/launch', ['launch/button_led_system.launch.py']),
-],
-
+    version='0.0.1',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/button_led_system.launch.py']),
+        ('share/' + package_name + '/config', ['config/ekf.yaml', 'config/RobotParams.yaml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='shaned75@googlemail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='Shane',
+    maintainer_email='your_email@example.com',
+    description='Clubbot2 robot package',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [ 
-             'mpu6050_node = clubbot2.mpu6050_node:main',
-             'button_publisher = clubbot2.button_publisher:main',
-             'led_subscriber = clubbot2.led_subscriber:main',
-             'button_to_led_bridge = clubbot2.button_to_led_bridge:main',
-             'esp32_reader = clubbot2.esp32_reader:main',
-             
+        'console_scripts': [
+            'esp32_reader = clubbot2.esp32_reader:main',
+            'button_publisher = clubbot2.button_publisher:main',
+            'button_to_led_bridge = clubbot2.button_to_led_bridge:main',
+            'led_subscriber = clubbot2.led_subscriber:main',
+            'mpu6050_node = clubbot2.mpu6050_node:main',
         ],
     },
 )
