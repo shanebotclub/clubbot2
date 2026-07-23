f1787c1818eee87dc1f4c36188b49e7fb21a6bf7
shanebotclub <shaned75@googlemail.com>
2026-07-23 16:40:04 +0100

    updated mc launch

--- launch/motor_controller.launch.py
+++ launch/motor_controller.launch.py
@@ -1,12 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('clubbot2')
+    params_file = os.path.join(pkg_share, 'config', 'RobotParams.yaml')
+
     return LaunchDescription([
         Node(
-            package='clubbot_motor_controller',
+            package='clubbot2',
             executable='motor_controller',
-            name='motor_controller',
+            parameters=[params_file],
             output='screen'
         )
     ])
