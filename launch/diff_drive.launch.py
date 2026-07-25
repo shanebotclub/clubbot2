abd4b352a3f7511a530e5be82b9cf3eade948ebe
shanebotclub <shaned75@googlemail.com>
2026-07-25 15:31:17 +0100

    x

--- launch/diff_drive.launch.py
+++ launch/diff_drive.launch.py
@@ -1,12 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory("clubbot2")
+    control_config = os.path.join(pkg_share, "config", "ros2_control.yaml")
+
     return LaunchDescription([
         Node(
             package="controller_manager",
             executable="ros2_control_node",
-            parameters=["config/ros2_control.yaml"],
+            parameters=[control_config],
             output="screen"
         ),
 
@@ -23,3 +28,4 @@ def generate_launch_description():
             output="screen"
         )
     ])
+
