eb382cbc6ea2c6b9c53180558ebe2ae8e16c3a45
shanebotclub <shaned75@googlemail.com>
2026-08-07 22:57:02 +0100

    pid tune

--- clubbot2/PID_motor_controller.py
+++ clubbot2/PID_motor_controller.py
@@ -99,8 +99,8 @@ class MotorController(Node):
         self.right_rpm_actual = 0.0
 
         # PID controllers
-        self.left_pid = PID(0.5, 0.1, 0.05)
-        self.right_pid = PID(0.5, 0.1, 0.05)
+        self.left_pid = PID(0.5, 0.03, 0.02)
+        self.right_pid = PID(0.5, 0.03, 0.02)
 
         # Subscribe to encoder ticks
         self.encoder_sub = self.create_subscription(
