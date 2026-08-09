30d92cb27e39fd9ccc3786ddda4f3f71e2410473
shanebotclub <shaned75@googlemail.com>
2026-08-09 16:46:58 +0100

    added pid to parameters

--- clubbot2/PID_motor_controller.py
+++ clubbot2/PID_motor_controller.py
@@ -46,34 +46,69 @@ class MotorController(Node):
     def __init__(self):
         super().__init__('motor_controller')
 
+        # ----------------------------
         # Declare parameters
-        self.declare_parameter('wheel_diameter')
-        self.declare_parameter('wheel_base')
-        self.declare_parameter('left_max_rpm')
-        self.declare_parameter('right_max_rpm')
-        self.declare_parameter('left_forward_pin')
-        self.declare_parameter('left_backward_pin')
-        self.declare_parameter('right_forward_pin')
-        self.declare_parameter('right_backward_pin')
-        self.declare_parameter('linear_scale', 1.0)
-        self.declare_parameter('angular_scale', 1.0)
+        # ----------------------------
+        self.declare_parameter('wheel_diameter', 0.08)
+        self.declare_parameter('wheel_base', 0.175)
 
+        self.declare_parameter('left_max_rpm', 107)
+        self.declare_parameter('right_max_rpm', 107)
+
+        self.declare_parameter('left_ticks_per_rotation', 620)
+        self.declare_parameter('right_ticks_per_rotation', 620)
+
+        self.declare_parameter('left_forward_pin', 18)
+        self.declare_parameter('left_backward_pin', 23)
+        self.declare_parameter('right_forward_pin', 24)
+        self.declare_parameter('right_backward_pin', 25)
+
+        self.declare_parameter('linear_scale', 0.4)
+        self.declare_parameter('angular_scale', 1.5)
+
+        # PID gains
+        self.declare_parameter('pid_left_p', 0.5)
+        self.declare_parameter('pid_left_i', 0.0)
+        self.declare_parameter('pid_left_d', 0.0)
+
+        self.declare_parameter('pid_right_p', 0.5)
+        self.declare_parameter('pid_right_i', 0.0)
+        self.declare_parameter('pid_right_d', 0.0)
+
+        # ----------------------------
         # Load parameters
+        # ----------------------------
         self.wheel_diameter = self.get_parameter('wheel_diameter').value
         self.wheel_base = self.get_parameter('wheel_base').value
+
         self.left_max_rpm = self.get_parameter('left_max_rpm').value
         self.right_max_rpm = self.get_parameter('right_max_rpm').value
+
+        self.left_ticks_per_rotation = self.get_parameter('left_ticks_per_rotation').value
+        self.right_ticks_per_rotation = self.get_parameter('right_ticks_per_rotation').value
 
         self.left_forward = self.get_parameter('left_forward_pin').value
         self.left_backward = self.get_parameter('left_backward_pin').value
         self.right_forward = self.get_parameter('right_forward_pin').value
         self.right_backward = self.get_parameter('right_backward_pin').value
+
         self.linear_scale = self.get_parameter('linear_scale').value
         self.angular_scale = self.get_parameter('angular_scale').value
 
+        # PID gains
+        lp = self.get_parameter('pid_left_p').value
+        li = self.get_parameter('pid_left_i').value
+        ld = self.get_parameter('pid_left_d').value
+
+        rp = self.get_parameter('pid_right_p').value
+        ri = self.get_parameter('pid_right_i').value
+        rd = self.get_parameter('pid_right_d').value
+
         self.get_logger().info("Motor controller parameters loaded.")
 
+        # ----------------------------
         # GPIO setup
+        # ----------------------------
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.left_forward, GPIO.OUT)
         GPIO.setup(self.left_backward, GPIO.OUT)
@@ -91,18 +126,24 @@ class MotorController(Node):
         self.rf_pwm.start(0)
         self.rb_pwm.start(0)
 
+        # ----------------------------
         # Encoder state
+        # ----------------------------
         self.last_left = 0
         self.last_right = 0
         self.last_time = time.time()
         self.left_rpm_actual = 0.0
         self.right_rpm_actual = 0.0
 
+        # ----------------------------
         # PID controllers
-        self.left_pid = PID(0.5, 0.00, 0.00)
-        self.right_pid = PID(0.5, 0.00, 0.00)
+        # ----------------------------
+        self.left_pid = PID(lp, li, ld)
+        self.right_pid = PID(rp, ri, rd)
 
-        # Subscribe to encoder ticks
+        # ----------------------------
+        # Subscriptions
+        # ----------------------------
         self.encoder_sub = self.create_subscription(
             Int32MultiArray,
             '/wheel_ticks',
@@ -110,7 +151,6 @@ class MotorController(Node):
             10
         )
 
-        # Subscribe to cmd_vel
         self.subscription = self.create_subscription(
             Twist,
             '/cmd_vel',
@@ -135,10 +175,8 @@ class MotorController(Node):
             dl = left - self.last_left
             dr = right - self.last_right
 
-            TICKS_PER_REV = 1024  # adjust if needed
-
-            self.left_rpm_actual = (dl / TICKS_PER_REV) / dt * 60.0
-            self.right_rpm_actual = (dr / TICKS_PER_REV) / dt * 60.0
+            self.left_rpm_actual = (dl / self.left_ticks_per_rotation) / dt * 60.0
+            self.right_rpm_actual = (dr / self.right_ticks_per_rotation) / dt * 60.0
 
         self.last_left = left
         self.last_right = right
